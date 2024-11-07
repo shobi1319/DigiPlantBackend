08fc85ab93692867b5899629ede55c8647b5c669
Muhammad Shoaib <muhammadshoaib.bscsf21@iba-suk.edu.pk>
2024-11-07 12:09:35 +0500

    updted

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const mongoURI = 'mongodb://localhost:27017/userDB'; // replace with your MongoDB connection string
+    const mongoURI = `mongodb+srv://${process.env.MONGO_USERNAME}:${encodeURIComponent(process.env.MONGO_PASSWORD)}@${process.env.MONGO_CLUSTER}/${process.env.MONGO_DB}?retryWrites=true&w=majority&appName=DigiPlant`;
     await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
     console.log('MongoDB connected');
   } catch (err) {
